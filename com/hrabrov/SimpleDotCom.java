0ca67f9df6ec3f904073af6a08c65159ba77c7e1
IngVarHrabroV <ingvarhrabrov@gmail.com>
2021-04-24 09:40:10 +0300

    feat: add test code for method checkYourself() that takes check user guess and returns result: hit, miss or kill

---
+++ com/hrabrov/SimpleDotCom.java
@@ -0,0 +1,14 @@
+package com.hrabrov;
+
+public class SimpleDotCom {
+    int[] locationCells;
+    int numOfHits = 0;
+
+    String checkYourself(String userGuess) {};
+
+    //setter method for reasign value variable 'locationCells'
+    void setLocationCells(int[] loc) {
+        locationCells = loc;
+    };
+
+}
---
+++ com/hrabrov/SimpleDotComGame.java
@@ -0,0 +1,6 @@
+package com.hrabrov;
+
+public class SimpleDotComGame {
+
+    void main();
+}
